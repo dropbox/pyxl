984ce74b107b82e47eb89b8396c106edeb61e1a3
Akhil Wable <akhil@dropbox.com>
2012-07-06 17:15:23 -0700

    wrap python babel extraction to work properly with pyxl

--- pyxl/codec/register.py
+++ pyxl/codec/register.py
@@ -25,7 +25,7 @@ def pyxl_transform(stream):
         print ex
         raise
 
-    return output
+    return output.strip()
 
 def pyxl_transform_string(text):
     stream = cStringIO.StringIO(text)
